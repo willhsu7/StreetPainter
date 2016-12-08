3ac5d766b42e189c015ac4a09f86d0e118d6638f
willhsu7 <willy04201991@gmail.com>
2016-12-08 17:54:03 -0500

    adding p5.scribble.js library
    draw 2D primitives in a sketchy look, switch to the line thickness

--- sketch.js
+++ sketch.js
@@ -1,24 +1,27 @@
 var can;
 var thewall;
 var c; //stroke colors 
+var lineSizeSlider; // switch the strokeWeight of drawing line
+
 
 //Google Maps API Key: AIzaSyAZl5MK8OWAPexNhbln2rcP1-quo856SRQ
 
 function preload() {
   can = loadImage('./assets/can_painter.png');
   thewall = loadImage('./assets/shanghai-bicycle-street-photography.jpg');
-} //load the can image
+} //load the images
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
   background(200);
   resetSketch(); // reset the canvas 
-  //image(can, 600, 0); 
   image(thewall, 0, 150);
-
   var button = createButton("clear the canvas"); //create the clear button
   button.mousePressed(resetSketch);
+  //create a slider(min, max, default value)
+  lineSizeSlider = createSlider(10, 70, 20);
+
+
 }
 
 function resetSketch() { // function to clear & reset the sketch canvas  
@@ -26,20 +29,21 @@ function resetSketch() { // function to clear & reset the sketch canvas
   canvas.position(0, 0);
   canvas.style('z-index', '-1');
   background(200);
-  //image(can, 600, 0);
   image(thewall, 0, 150);
 }
 
 function draw() {
   //background(51);
-  // display instructions
+  //display instructions
   noStroke();
   fill(200);
   rect(0, 100, 1144, 50);
   fill(0);
   text("Draw whatever you love! Press the keys on your keyboard to change the colors, have fun!", 10, 130);
   textSize(20);
+  //image(thewall, 0, 150);
   //image(can, mouseX, mouseY);
+
   /*ellipse(mouseX, mouseY,26, 26);
   can.position.x = mouseX;
   can.position.y = mouseY;
@@ -50,13 +54,21 @@ function draw() {
 }
 
 function mouseDragged() {
-  line(pmouseX, pmouseY, mouseX, mouseY);
-  strokeWeight(30);
-  stroke(c);
+  var scribble = new Scribble(); // Create an instance: p5.scribble.js library
+  scribble.bowing = 2; // changes the bowing of lines
+  scribble.roughness = 4; // changes the roughness of lines
+  scribble.scribbleLine(pmouseX, pmouseY, mouseX, mouseY);
+
+  //line(pmouseX, pmouseY, mouseX, mouseY);
+  //ellipse(mouseX, mouseY, 15, 15);
+  //strokeWeight(30);
+  strokeWeight(lineSizeSlider.value());
+  stroke(c, 0.5);
+
 }
 
-function keyPressed(){
-  c= color(random(0, 255), random(0, 255), random(0, 255));
+function keyPressed() { //changing colors 
+  c = color(random(0, 255), random(0, 255), random(0, 255));
 }
 
 /*function keyPressed() {
@@ -68,23 +80,5 @@ function keyPressed(){
   }
   if (key == 'g' || key == 'G') { //green
     c = color(46, 218, 26);
-  }
-  if (key == 'o' || key == 'O') { //orange
-    c = color(255, 82, 2);
-  }
-  if (key == 'p' || key == 'P') { //purple
-    c = color(186, 21, 207);
-  }
-  if (key == 'y' || key == 'Y') { //yellow
-    c = color(255, 255, 0);
-  }
-  if (key == 'f' || key == 'F') { //gray
-    c = color(96, 96, 96);
-  }
-  if (key == 'w' || key == 'W') { //white
-    c = color(255);
-  }
-  if (key == 'j' || key == 'J') { //black
-    c = color(0);
   }
 }*/
