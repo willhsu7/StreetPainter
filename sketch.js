22f4030832f0d9de51bb812ca78ae83946e0b092
willhsu7 <willy04201991@gmail.com>
2016-12-14 23:59:43 -0500

    Dec14_saveCanvas_soundAdding

--- sketch.js
+++ sketch.js
@@ -2,26 +2,33 @@ var can;
 var thewall;
 var c; //stroke colors 
 var lineSizeSlider; // switch the strokeWeight of drawing line
-
+var snapbutton;
+var grafsound;
 
 //Google Maps API Key: AIzaSyAZl5MK8OWAPexNhbln2rcP1-quo856SRQ
 
 function preload() {
   can = loadImage('./assets/can_painter.png');
   thewall = loadImage('./assets/shanghai-bicycle-street-photography.jpg');
-} //load the images
+  grafsound = loadSound('./assets/GRAFFITI_SOUND.mp3');
+} //load the images & audio 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(200);
+
   resetSketch(); // reset the canvas 
-  image(thewall, 0, 150);
+  image(thewall, 0, 350);
   var button = createButton("clear the canvas"); //create the clear button
   button.mousePressed(resetSketch);
+  button.size(100, 50);
   //create a slider(min, max, default value)
   lineSizeSlider = createSlider(10, 70, 20);
 
-
+  snapbutton = createButton('snap the street view');
+  snapbutton.position(0, 650);
+  snapbutton.size(100, 50);
+  snapbutton.mousePressed(snapCanvas);
 }
 
 function resetSketch() { // function to clear & reset the sketch canvas  
@@ -29,12 +36,17 @@ function resetSketch() { // function to clear & reset the sketch canvas
   canvas.position(0, 0);
   canvas.style('z-index', '-1');
   background(200);
-  image(thewall, 0, 150);
+  image(thewall, 0, 350);
+}
+
+function snapCanvas() {
+  saveCanvas('Streetview', 'jpg');
 }
 
 function draw() {
   //background(51);
   //display instructions
+  //image(thewall, 0, 350);
   noStroke();
   fill(200);
   rect(0, 100, 1144, 50);
@@ -42,6 +54,7 @@ function draw() {
   text("Draw whatever you love! Press the keys on your keyboard to change the colors, have fun!", 10, 130);
   textSize(20);
   //image(thewall, 0, 150);
+
   //image(can, mouseX, mouseY);
 
   /*ellipse(mouseX, mouseY,26, 26);
@@ -58,13 +71,18 @@ function mouseDragged() {
   scribble.bowing = 2; // changes the bowing of lines
   scribble.roughness = 4; // changes the roughness of lines
   scribble.scribbleLine(pmouseX, pmouseY, mouseX, mouseY);
-
   //line(pmouseX, pmouseY, mouseX, mouseY);
   //ellipse(mouseX, mouseY, 15, 15);
   //strokeWeight(30);
   strokeWeight(lineSizeSlider.value());
   stroke(c, 0.5);
+}
 
+function mousePressed(){   //when mouse is pressed trigger graffiti sound
+  grafsound.setVolume(0.7);
+  grafsound.play();
+  //grafsound.loop();
+  grafsound.jump(11, 1); // jump(cueTime of the soundFile in seconds, duration in seconds)
 }
 
 function keyPressed() { //changing colors 
